716e0f83374f81ae8b0a2bc152dee2f6bb1f85f3
Yuval Allweil <themarquisdesheric@gmail.com>
2019-11-25 22:01:28 -0800

    create basic getComputedProperties utils

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import getComputedProperties from './utils';
+
+const node = document.querySelector('div');
+
+console.log(getComputedProperties(node));
---
+++ src/utils.js
@@ -0,0 +1,34 @@
+// to string
+const getComputedPropertiesToString = (node) => {
+  const styleDeclaration = window.getComputedStyle(node, null);
+  let computedProperties = '';
+
+  for (let i = 0; i < styleDeclaration.length; i++) { 
+    const cssProperty = styleDeclaration.item(i);
+    
+    computedProperties += cssProperty + ': ' + styleDeclaration.getPropertyValue(cssProperty) + ';\n';
+  };
+
+  return computedProperties;
+};
+
+// to string, alternate way that might be better
+const getComputedPropertiesFromCssText = (node) => 
+  window.getComputedStyle(node, null).cssText.replace(/;/g, ';\n');
+
+// to object
+const getComputedProperties = (node) => {
+  const styleDeclaration = window.getComputedStyle(node, null);
+  let computedProperties = {};
+
+  for (let i = 0; i < styleDeclaration.length; i++) { 
+    const cssProperty = styleDeclaration.item(i);
+    const cssPropertyValue = styleDeclaration.getPropertyValue(cssProperty);
+    
+    computedProperties[cssProperty] = cssPropertyValue;
+  };
+
+  return computedProperties;
+};
+
+export default getComputedProperties;
